358e3d27c0de60e89ba1391f38dd5486201760a1
Matty Reed <mattyreed1@gmail.com>
2024-09-21 17:57:39 -0700

    fix ui

--- Code.js
+++ Code.js
@@ -61,3 +61,7 @@ function getSelectedText() {
 function getScriptContent(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
+
+function getCssContent() {
+  return HtmlService.createHtmlOutputFromFile('ui/CSS').getContent();
+}
